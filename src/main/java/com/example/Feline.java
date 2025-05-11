5db6a02d946b25cf8b948c40bd7851e6327370b5
ROMAN MEDVEDEV <roman.medvedev@bk.ru>
2025-05-11 13:30:11 +0300

    First commit

--- src/main/java/com/example/Feline.java
+++ src/main/java/com/example/Feline.java
@@ -7,6 +7,7 @@ public class Feline extends Animal implements Predator {
     @Override
     public List<String> eatMeat() throws Exception {
         return getFood("Хищник");
+
     }
 
     @Override
@@ -23,3 +24,4 @@ public class Feline extends Animal implements Predator {
     }
 
 }
+
